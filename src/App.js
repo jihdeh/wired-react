6604735a226604d3522b383020ab04b341f3ca9f
Babajide Fowotade <jide.b.tade@gmail.com>
2021-08-21 19:51:04 +0100

    feat: implement component refactor

--- src/App.js
+++ src/App.js
@@ -1,46 +1,51 @@
-import * as React from "react";
-import { Switch } from "./switch";
+import React, { useState } from "react";
 import {
   SettingRow,
-  SettingRowContent,
   SettingRowActions,
-  SettingRowTitle,
+  SettingRowContent,
+  SettingRowTitle
 } from "./settingRow";
+import { Switch } from "./switch";
 
-function Toggler({ children }) {
-  // todo, implement this re-usable component.
-  return children;
+function Toggler({ Component }) {
+  const [checked, setChecked] = useState(true);
+  const onClick = () => setChecked(!checked);
+
+  return <Component onClick={onClick} checked={checked} />;
 }
 
-function TogglerOn({ children }) {
-  // todo, implement this re-usable component.
-  return children;
+function TogglerOn({ children, checked }) {
+  return checked && children;
 }
 
-function TogglerOff({ children }) {
-  // todo, implement this re-usable component.
-  return children;
+function TogglerOff({ children, checked }) {
+  return checked && children;
 }
 
-function TogglerButton() {
-  // todo, implement this re-usable component.
-  const toDoSwitchProps = { on: true, onClick: null };
+function TogglerButton({onClick, checked}) {
+  const toDoSwitchProps = { on: checked, onClick };
   return <Switch {...toDoSwitchProps} />;
+}
+
+function Content({onClick, checked}) {
+  return (
+    <>
+      <SettingRowContent>
+        <SettingRowTitle>Some setting</SettingRowTitle>
+        <TogglerOn checked={checked}>Is enabled</TogglerOn>
+        <TogglerOff checked={!checked}>Is disabled</TogglerOff>
+      </SettingRowContent>
+      <SettingRowActions>
+        <TogglerButton onClick={onClick} checked={checked} />
+      </SettingRowActions>
+    </>
+  );
 }
 
 function App() {
   return (
     <SettingRow>
-      <Toggler>
-        <SettingRowContent>
-          <SettingRowTitle>Some setting</SettingRowTitle>
-          <TogglerOn>Is enabled</TogglerOn>
-          <TogglerOff>Is disabled</TogglerOff>
-        </SettingRowContent>
-        <SettingRowActions>
-          <TogglerButton />
-        </SettingRowActions>
-      </Toggler>
+      <Toggler Component={Content} />
     </SettingRow>
   );
 }
